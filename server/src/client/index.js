c162388ba4a99bdf492cb63296ca705f52156579
thsoon <dlatns0201@gmail.com>
2021-02-23 15:33:34 +0900

    add button event and render once although client side renders

--- server/src/client/index.js
+++ server/src/client/index.js
@@ -1,1 +1,8 @@
-console.log('it is client side rendering');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Home from './components/Home';
+
+// React.render 를 사용하면 서버에서 그려준 것을 브라우저가 한 번 더 그려준다.
+// 그래서 그려준 것을 재랜더링하지 않고 이벤트만 등록해주는 React.hydrate 를 사용한다.
+ReactDOM.hydrate(<Home />, document.getElementById('root'));
