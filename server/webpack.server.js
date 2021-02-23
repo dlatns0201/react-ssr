52fb4f8fc0a968e48d16d379fc288faba74cfb49
thsoon <dlatns0201@gmail.com>
2021-02-23 16:17:34 +0900

    unusing module in importing module excluded with webpack-node-externals

--- server/webpack.server.js
+++ server/webpack.server.js
@@ -1,5 +1,7 @@
 const path = require('path');
 const merge = require('webpack-merge');
+const webpackNodeExtenrals = require('webpack-node-externals');
+
 const baseConfig = require('./webpack.base');
 
 const config = {
@@ -9,7 +11,13 @@ const config = {
   output: {
     filename: 'bundle.js',
     path: path.resolve('build')
-  }
+  },
+
+  externals: [
+    // express 등 외부 라이브러리를 사용할 때 코드에 사용된 것 외의 의존하는 모듈들을 제거해준다.
+    // 따라서, 번들링할 때 속도가 빨라지게 만들어준다.
+    webpackNodeExtenrals()
+  ]
 };
 
 module.exports = merge(baseConfig, config);
