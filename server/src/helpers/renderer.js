d2e33b477b8f88ebf2d17e7d1c98613060d2031c
thsoon <dlatns0201@gmail.com>
2021-02-24 17:29:40 +0900

    prevents xss attack at setting initial-state in server

--- server/src/helpers/renderer.js
+++ server/src/helpers/renderer.js
@@ -3,6 +3,7 @@ import { renderToString } from 'react-dom/server';
 import { Provider } from 'react-redux';
 import { StaticRouter } from 'react-router-dom';
 import { renderRoutes } from "react-router-config";
+import serialize from 'serialize-javascript';
 
 import Routes from '../client/Routes';
 
@@ -21,7 +22,7 @@ export default (req, store) => {
       <body>
         <div id="root">${content}</div>
         <script>
-          window.__INITIAL_STATE__ = ${JSON.stringify(store.getState())}
+          window.__INITIAL_STATE__ = ${serialize(store.getState())}
         </script>
         <script src="bundle.js"></script>
       </body>
