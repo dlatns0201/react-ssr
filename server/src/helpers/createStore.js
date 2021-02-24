25c56c0e7796462a5b39615c1eef8d9160c46180
thsoon <dlatns0201@gmail.com>
2021-02-24 18:44:26 +0900

    add axios instance for server fetching through thunk

--- server/src/helpers/createStore.js
+++ server/src/helpers/createStore.js
@@ -1,10 +1,15 @@
 import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
+import axios from "axios";
 
 import reducers from "../client/reducers";
 
-export default (initialState) => {
-  const store = createStore(reducers, initialState || {}, applyMiddleware(thunk));
+export default (req) => {
+  const api = axios.create({
+    baseURL: 'https://react-ssr-api.herokuapp.com',
+    headers: { cookie: req.get('cookie') || '' }
+  })
+  const store = createStore(reducers, {}, applyMiddleware(thunk.withExtraArgument(api)));
 
   return store;
 }
